4f0aed2ded169cc0ad01f4f48abbaf8f1cff42e4
nadjano <nnolte@ebi.ac.uk>
2025-06-23 10:50:24 +0200

    Add aggregate_transcripts_to_genes to __all__ for module export

--- polyase/__init__.py
+++ polyase/__init__.py
@@ -13,7 +13,7 @@ from .allele_utils import AlleleRatioCalculator, calculate_allelic_ratios
 from .multimapping import MultimappingRatioCalculator, calculate_multi_ratios
 from .filter import filter_low_expressed_genes
 from .plotting import plot_allelic_ratios, plot_allelic_ratios_comparison, plot_top_differential_syntelogs
-from .ase_data_loader import load_ase_data
+from .ase_data_loader import load_ase_data, aggregate_transcripts_to_genes
 from .stats import test_allelic_ratios_between_conditions, test_allelic_ratios_within_conditions, get_top_differential_syntelogs
 
-__all__ = ["filter_low_expressed_genes","AlleleRatioCalculator", "calculate_allelic_ratios", "MultimappingRatioCalculator", "calculate_multi_ratios","plot_allelic_ratios", "plot_allelic_ratios_comparison", "load_ase_data", "test_allelic_ratios_between_conditions", "test_allelic_ratios_within_conditions","plot_top_differential_syntelogs", "get_top_differential_syntelogs"]
+__all__ = ["filter_low_expressed_genes","AlleleRatioCalculator", "calculate_allelic_ratios", "MultimappingRatioCalculator", "calculate_multi_ratios","plot_allelic_ratios", "plot_allelic_ratios_comparison", "load_ase_data", "aggregate_transcripts_to_genes", "test_allelic_ratios_between_conditions", "test_allelic_ratios_within_conditions","plot_top_differential_syntelogs", "get_top_differential_syntelogs"]

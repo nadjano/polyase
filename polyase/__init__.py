9c4dca9338deed583aa925dc2ba39e3dece0ef3e
nadjano <nnolte@ebi.ac.uk>
2026-02-24 16:40:34 +0100

    update version

--- polyase/__init__.py
+++ polyase/__init__.py
@@ -6,7 +6,7 @@ visualizing allele-specific expression patterns, and statistical testing
 of allelic imbalance in polyploid plant genomes.
 """
 
-__version__ = "1.3.0"
+__version__ = "1.3.1"
 __author__ = "Nadja Nolte"
 
 from .allele_utils import AlleleRatioCalculator, calculate_allelic_ratios
